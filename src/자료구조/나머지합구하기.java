536b6f645f3bbd4f53ed766469516e7f565cf398
youngsuh <yseokim0212@gmail.com>
2025-07-01 16:24:21 +0900

    [refactor] 나머지합 구하기

--- src/자료구조/나머지합구하기.java
+++ src/자료구조/나머지합구하기.java
@@ -12,20 +12,21 @@ public class 나머지합구하기 {
         int N = Integer.parseInt(stringTokenizer.nextToken());
         int M = Integer.parseInt(stringTokenizer.nextToken());
 
-        int[] S = new int[N];
+        long[] S = new long[N];
+        long[] C = new long[M];
+        long answer = 0;
+
         stringTokenizer = new StringTokenizer(bf.readLine());
-        S[0] = Integer.parseInt(stringTokenizer.nextToken());
+        S[0] = Long.parseLong(stringTokenizer.nextToken());
 
         // 합 배열 입력받기
         for(int i=1 ; i<N ; i++){
-            S[i] = S[i-1] + Integer.parseInt(stringTokenizer.nextToken());
+            S[i] = S[i-1] + Long.parseLong(stringTokenizer.nextToken());
         }
 
-        int[] C = new int[N];
-        int answer = 0;
         // 합 배열 업데이트
         for(int i = 0 ; i<N ; i++) {
-            int remainder = (S[i] % M);
+            int remainder = (int) (S[i] % M);
 
             if(remainder == 0) answer ++;
             C[remainder]++;
@@ -33,7 +34,7 @@ public class 나머지합구하기 {
         }
 
         for(int i=0 ; i<M ; i++){
-            if(C[i] > 0) {
+            if(C[i] > 1) {
                 answer = answer + ( C[i] * (C[i]-1) /2 );
             }
         }
