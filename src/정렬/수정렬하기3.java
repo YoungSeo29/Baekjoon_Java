f0875b0129b1b4bba3f7729523f1ee9ac356e6b3
youngsuh <yseokim0212@gmail.com>
2025-08-06 02:52:39 +0900

    [create] 수정렬하기3

---
+++ src/정렬/수정렬하기3.java
@@ -0,0 +1,64 @@
+package 정렬;
+
+import java.io.*;
+
+public class 수정렬하기3 {
+    public static int[] A;
+    public static long result;
+
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        A = new int[N];
+
+        for (int i=0 ; i< N ; i++) {
+            A[i] = Integer.parseInt(br.readLine());
+        }
+
+        br.close();
+
+        Radix_sort(A, 5);
+
+        for (int i=0 ; i< N ; i++) {
+            bw.write(A[i] + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+
+    public static void Radix_sort(int[] A, int max_size) {
+        int[] output = new int[A.length];
+        int jarisu = 1;
+        int count = 0;
+
+        // 최대 자리수만큼 반복
+        while (count != max_size) {
+            int[] bucket = new int[10];
+
+            for (int i=0 ; i<A.length ; i++) {
+                bucket[(A[i] / jarisu) % 10]++;  // 일의 자리부터 시작
+            }
+
+            for (int i=1 ; i<10 ; i++) {
+                bucket[i] += bucket[i-1];
+            }
+
+            for (int i=A.length -1 ; i>=0 ; i--) {
+                output[bucket[(A[i] / jarisu % 10)]-1] = A[i];
+                bucket[(A[i] / jarisu) % 10]--;
+            }
+
+            // 뒤에서부터 정렬
+            for(int i=0 ; i<A.length ; i++) {
+                // 다음 자리수를 이동하기 위해 현재 자리수 기준 정렬 데이터 저장
+                A[i] = output[i];
+            }
+
+            jarisu = jarisu * 10;  // 자리수 증가시키기
+            count++;
+        }
+    }
+}
